21feb681ef3b56a59d03503e999c7842775402cd
PersonTheCat <professionallyperson@gmail.com>
2022-06-14 09:49:50 -0500

    [optimization] partial heightmap generation

--- src/main/java/personthecat/roadmap/HeightmapGenerator.java
+++ src/main/java/personthecat/roadmap/HeightmapGenerator.java
@@ -14,7 +14,11 @@ public class HeightmapGenerator {
     private FastNoise generator;
     private int xOffset = 0;
     private int yOffset = 0;
+    private int prevXOffset = 0;
+    private int prevYOffset = 0;
     private int seed;
+    private int prevSeed;
+    private BufferedImage prevImage;
 
     public HeightmapGenerator(final Config config, final int seed) {
         this.config = config;
@@ -31,33 +35,65 @@ public class HeightmapGenerator {
     }
 
     public void up(final int count) {
-        this.yOffset -= 16 * count;
+        this.yOffset -= 32 * count;
     }
 
     public void down(final int count) {
-        this.yOffset += 16 * count;
+        this.yOffset += 32 * count;
     }
 
     public void left(final int count) {
-        this.xOffset -= 16 * count;
+        this.xOffset -= 32 * count;
     }
 
     public void right(final int count) {
-        this.xOffset += 16 * count;
+        this.xOffset += 32 * count;
     }
 
     public BufferedImage generate() {
         final int w = this.config.getChunkWidth() << 4;
         final int h = this.config.getChunkHeight() << 4;
         final BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        if (this.prevImage == null
+                || this.prevImage.getWidth() != image.getWidth()
+                || this.prevImage.getHeight() != image.getHeight()
+                || this.seed != this.prevSeed ) {
+            this.writeNewImage(image);
+        } else {
+            this.writePartialImage(image);
+        }
+        this.prevXOffset = this.xOffset;
+        this.prevYOffset = this.yOffset;
+        this.prevSeed = this.seed;
+        this.prevImage = image;
+        return image;
+    }
 
-        for (int x = 0; x < w; x++) {
-            for (int y = 0; y < h; y++) {
+    private void writeNewImage(final BufferedImage image) {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
                 final float n = this.generator.getNoiseScaled(x + this.xOffset, y + this.yOffset);
                 image.setRGB(x, y, this.getColor(x, y, n).getRGB());
             }
         }
-        return image;
+    }
+
+    private void writePartialImage(final BufferedImage image) {
+        final int xD = this.xOffset - this.prevXOffset;
+        final int yD = this.yOffset - this.prevYOffset;
+
+        for (int x = 0; x < image.getWidth(); x++) {
+            final int xO = x + xD;
+            for (int y = 0; y < image.getHeight(); y++) {
+                final int yO = y + yD;
+                if (xO >= 0 && xO < image.getWidth() && yO >= 0 && yO < image.getHeight()) {
+                    image.setRGB(x, y, this.prevImage.getRGB(xO, yO));
+                } else {
+                    final float n = this.generator.getNoiseScaled(x + this.xOffset, y + this.yOffset);
+                    image.setRGB(x, y, this.getColor(x, y, n).getRGB());
+                }
+            }
+        }
     }
 
     private Color getColor(final int x, final int y, final float n) {
