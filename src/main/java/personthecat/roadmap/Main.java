ff63996dcd30331a3e15592f3b40b0237a5c6445
PersonTheCat <professionallyperson@gmail.com>
2024-02-07 14:27:18 -0600

    Optimize config reloading and saving

--- src/main/java/personthecat/roadmap/Main.java
+++ src/main/java/personthecat/roadmap/Main.java
@@ -26,20 +26,24 @@ public class Main {
         void run() {
             final var window = new AppWindow(this.config, this.tracker, this.createNextImage(true, true));
             window.onKeyPressed(KeyEvent.VK_SPACE, w -> w.render(this.createNextImage(false, true)));
-            window.onKeyPressed('r', w -> w.render(this.createNextImage(true, false)));
             window.onKeyPressed(KeyEvent.VK_ESCAPE, AppWindow::close);
 
+            window.onKeyPressed('r', w -> {
+                this.reloadConfig();
+                w.render(this.createNextImage(true, false));
+            });
             window.onKeyPressed('m', w -> {
                 this.tracker.toggleMountains();
                 w.render(this.createNextImage(true, false));
             });
-            window.onKeyPressed('s', (w, e) -> {
+            window.onKeyPressed(KeyEvent.VK_S, (w, e) -> {
                 if (e.isControlDown()) {
-                    this.config.save();
-                } else {
-                    this.tracker.toggleSideView();
-                    w.render(this.createNextImage(true, false));
+                    this.config.saveIfUpdated(this.tracker);
                 }
+            });
+            window.onKeyPressed('s', w -> {
+                this.tracker.toggleSideView();
+                w.render(this.createNextImage(false, false));
             });
             window.onKeyPressed(new int[] { KeyEvent.VK_EQUALS, KeyEvent.VK_PLUS }, (w, e) -> {
                 if (e.isControlDown()) {
@@ -78,36 +82,39 @@ public class Main {
 
         BufferedImage createNextImage(final boolean reload, final boolean newSeed) {
             if (reload) {
-                final int oH = this.config.getChunkHeight();
-                final int oW = this.config.getChunkWidth();
-                final boolean oSideView = this.config.isSideView();
-                final boolean oMountains = this.config.isMountains();
-                final float oZoom = this.config.getZoom();
-                final float oSideViewAngle = this.config.getSideViewAngle();
-                this.config.reloadFromDisk();
-                if (oH != this.config.getChunkHeight() || oW != this.config.getChunkWidth()) {
-                    if (this.window != null) {
-                        this.window.pack();
-                    }
-                }
-                if (oSideView != this.config.isSideView()) {
-                    this.tracker.setSideView(this.config.isSideView());
-                }
-                if (oMountains != this.config.isMountains()) {
-                    this.tracker.setMountains(this.config.isMountains());
-                }
-                if (oZoom != this.config.getZoom()) {
-                    this.tracker.setZoom(this.config.getZoom());
-                }
-                if (oSideViewAngle != this.config.getSideViewAngle()) {
-                    this.tracker.setSideViewAngle(this.config.getSideViewAngle());
-                }
                 this.generator.reload();
             }
             if (newSeed) {
                 this.generator.next(this.rand.nextInt());
             }
             return this.generator.generate(reload);
+        }
+
+        void reloadConfig() {
+            final int oH = this.config.getChunkHeight();
+            final int oW = this.config.getChunkWidth();
+            final boolean oSideView = this.config.isSideView();
+            final boolean oMountains = this.config.isMountains();
+            final float oZoom = this.config.getZoom();
+            final float oSideViewAngle = this.config.getSideViewAngle();
+            this.config.reloadFromDisk();
+            if (oH != this.config.getChunkHeight() || oW != this.config.getChunkWidth()) {
+                if (this.window != null) {
+                    this.window.pack();
+                }
+            }
+            if (oSideView != this.config.isSideView()) {
+                this.tracker.setSideView(this.config.isSideView());
+            }
+            if (oMountains != this.config.isMountains()) {
+                this.tracker.setMountains(this.config.isMountains());
+            }
+            if (oZoom != this.config.getZoom()) {
+                this.tracker.setZoom(this.config.getZoom());
+            }
+            if (oSideViewAngle != this.config.getSideViewAngle()) {
+                this.tracker.setSideViewAngle(this.config.getSideViewAngle());
+            }
         }
     }
 }
